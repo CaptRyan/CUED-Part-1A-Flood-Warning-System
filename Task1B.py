060d1203ba029fd23ea9a0c4f4d64ced2a6d4b4e
CaptRyan <65838091+CaptRyan@users.noreply.github.com>
2023-01-19 17:12:56 +0000

    Completion of Task1B(Lack of Documentation))
    Completed the Task1B without necessary documentation, updated the file pythonapp.yml for trial testing

---
+++ Task1B.py
@@ -0,0 +1,20 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+from floodsystem.utils import sorted_by_key
+
+
+def run():
+    p =  (52.2053, 0.1218)
+    smallest_ten =[]
+    biggest_ten = []
+
+    stations = build_station_list()
+    distance_list = stations_by_distance(stations, p)
+    for i in range(0, 10):
+        smallest_ten.append(distance_list[i])
+        biggest_ten.append(distance_list[-1-i])
+    print (smallest_ten, "\n", sorted_by_key(biggest_ten, 1, False))
+
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,17 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+
+def stations_by_distance(stations, p):
+    distance_list = []
+    coord_p = p
+    for i in stations:
+        coord_s = i.coord
+        distance = haversine(coord_s, coord_p)
+        distance_list.append((i.name, distance))
+
+    distance_list = sorted_by_key(distance_list, 1)
+    
+    return distance_list
