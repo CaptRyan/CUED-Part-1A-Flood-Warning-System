f9ada83645ba93db3c023fd8248698f517a17297
CaptRyan <65838091+CaptRyan@users.noreply.github.com>
2023-01-19 17:52:46 +0000

    Added comments for Task1B, but no test have been implemented for geo.py

--- Task1B.py
+++ Task1B.py
@@ -4,15 +4,25 @@ from floodsystem.utils import sorted_by_key
 
 
 def run():
+    # Coordinate at Cambridge City Centre
     p = (52.2053, 0.1218)
+
+    # Empty lists to hold the nearest and furthest 10 stations
     smallest_ten = []
     biggest_ten = []
 
+    # Build list of stations
     stations = build_station_list()
+
+    # Calculate the distance between the Coordinate p and each station
     distance_list = stations_by_distance(stations, p)
+
+    # Append information to empty list
     for i in range(0, 10):
         smallest_ten.append(distance_list[i])
         biggest_ten.append(distance_list[- 1 - i])
+
+    # Display obtained lists
     print(smallest_ten, sorted_by_key(biggest_ten, 1, False))
 
 
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,14 +10,18 @@ from .utils import sorted_by_key  # noqa
 from haversine import haversine
 
 
+# Calculating the distance between the coordinate p and each station
 def stations_by_distance(stations, p):
     distance_list = []
     coord_p = p
+
+    # Calculating distance via haversine and store in a list
     for i in stations:
         coord_s = i.coord
         distance = haversine(coord_s, coord_p)
         distance_list.append((i.name, distance))
 
+    # Sort the list from nearest to furthest distances
     distance_list = sorted_by_key(distance_list, 1)
 
     return distance_list
