21d8f91f15b4f9e6c22261734f9bcb6eb95966f7
MatthewDux <matthewduxbury@icloud.com>
2023-01-20 19:08:40 +0000

    Created file for task 1E and 1F

---
+++ Task1E.py
@@ -0,0 +1,6 @@
+
+
+
+if __name__ == "__main__":
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
+    run()
---
+++ Task1F.py
@@ -0,0 +1,6 @@
+
+
+
+if __name__ == "__main__":
+    print("*** Task 1F: CUED Part IA Flood Warning System ***")
+    run()
