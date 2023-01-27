dbfcca605d48f78093ab15f4f5b2cf0155d6ee9c
MatthewDux <matthewduxbury@icloud.com>
2023-01-27 10:43:10 +0000

    implemented testing for task 1F

--- test_station.py
+++ test_station.py
@@ -25,3 +25,18 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+
+def test_typical_range_consistent():
+
+    # Create a station
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+    
+    assert s.typical_range_consistent is True
